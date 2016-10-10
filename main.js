34465783e1e062351caf88bbd78360fb07072e5c
sundial778 <amcclintock@atl.yearup.org>
2016-10-10 08:44:14 -0400

    Fixed toggle issue

--- main.js
+++ main.js
@@ -1,55 +1,88 @@
 $(document).ready(function() {
-	
-	function showText(id)
-	{
-		$(id).find("p").slideToggle()
 
+
+
+     var windowWidth = $(window).width();
+    var desktopMode = false;
+    var mobileMode = false;
+    
+    $(window).resize(function() {
+  
+   location.reload();
+   windowWidth = $(window).width()
+});
+    
+    if (windowWidth > 990) {
+
+        desktopMode = true;
+        mobileMode = false;
+    } else {
+        mobileMode = true;
+        desktopMode = false;
     }
-	
-	$('#projects').click(function() {
-    	
-    	showText("#projects");
-	});
+
+    function showText(id)
+
+    {
+        if (mobileMode) {
+
+            $(id).find("p").slideToggle()
+        } else {
+            return mobileMode
+        }
+    }
+
+    $('#projects').click(function() {
+
+        showText("#projects");
+    });
 
 
-$('#life').click(function() {
-    	
-    	showText("#life");
-	});
-$('#work').click(function() {
-    	
-    	showText("#work");
-	});
-$('#play').click(function() {
-    	
-    	showText("#play");
-	});
-function fadeIn(id)
-	{
-		$('#contentChanger.inner').replaceWith($(id))
-$('#projects').hide()
-$('#play').hide()
-$('#work').hide()
-$('#life').hide()
-		$(id).appendTo( $( '#contentChanger' ));
+    $('#life').click(function() {
 
-$(id).stop(true,true).animate({right:'=600',opacity:'show'},1000);
+        showText("#life");
+    });
+    $('#work').click(function() {
+
+        showText("#work");
+    });
+    $('#play').click(function() {
+
+        showText("#play");
+    });
+
+    function fadeIn(id) {
+        if (desktopMode) {
+            $('#contentChanger.inner').replaceWith($(id))
+
+            $('#projects').hide()
+            $('#play').hide()
+            $('#work').hide()
+            $('#life').hide()
+            $(id).appendTo($('#contentChanger'));
+
+            $(id).stop(true, true).animate({
+                right: '=600',
+                opacity: 'show'
+            }, 1000);
+        } else {
+            return desktopMode;
+        }
     }
     $('#plink').click(function() {
-    	
-    	fadeIn("#projects");
-	});
-	$('#playLink').click(function() {
-    	
-    	fadeIn("#play");
-	});
-	$('#wlink').click(function() {
-    	
-    	fadeIn("#work");
-	});
-	$('#llink').click(function() {
-    	
-    	fadeIn("#life");
-	});
-});
 
+        fadeIn("#projects");
+    });
+    $('#playLink').click(function() {
+
+        fadeIn("#play");
+    });
+    $('#wlink').click(function() {
+
+        fadeIn("#work");
+    });
+    $('#llink').click(function() {
+
+        fadeIn("#life");
+    });
+});
